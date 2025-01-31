30a22325a55bbc4dfdaaef637a11460f571c33ad
fbotel17 <faustbtl17@gmail.com>
2025-01-31 10:24:13 +0100

    page connexion + navbar

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -12,10 +12,7 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(MedicamentRepository $medicamentRepository): Response
     {
-        $medicaments = $medicamentRepository->findBy([], ['id' => 'DESC'], 100);
 
-        return $this->render('home/index.html.twig', [
-            'medicaments' => $medicaments,
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
