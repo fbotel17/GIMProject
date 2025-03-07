7ee1b483a47a03c648a59c2bfe8a364eda72f4f6
fbotel17 <faustbtl17@gmail.com>
2025-03-07 15:28:05 +0100

    commande deduire medicament traitement

--- GIM/migrations/Version20250307135725.php
+++ GIM/migrations/Version20250307135725.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250225104621 extends AbstractMigration
+final class Version20250307135725 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20250225104621 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE inventaire DROP nb_pilules_par_boite, DROP nb_pilules_restantes');
+        $this->addSql('ALTER TABLE inventaire ADD nb_boite INT NOT NULL');
+        $this->addSql('ALTER TABLE medicament ADD stock INT NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE inventaire ADD nb_pilules_par_boite INT NOT NULL, ADD nb_pilules_restantes INT NOT NULL');
+        $this->addSql('ALTER TABLE inventaire DROP nb_boite');
+        $this->addSql('ALTER TABLE medicament DROP stock');
     }
 }
---
+++ GIM/src/Command/DeduireMedicamentsCommand.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Inventaire;
+use App\Entity\Traitement;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'app:deduire-medicaments',
+    description: 'Add a short description for your command',
+)]
+class DeduireMedicamentsCommand extends Command
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $traitements = $this->entityManager->getRepository(Traitement::class)->findBy(['actif' => true]);
+
+        foreach ($traitements as $traitement) {
+            $user = $traitement->getUser();
+            $frequence = $traitement->getFrequence();
+            $dose = $traitement->getDose();
+
+            if ($frequence === 'jour') {
+                $this->deduireMedicaments($traitement, $dose);
+            } elseif ($frequence === 'semaine') {
+                $jourDeLaSemaine = (new \DateTime())->format('N');
+                if ($jourDeLaSemaine === '1') { // Lundi
+                    $this->deduireMedicaments($traitement, $dose);
+                }
+            }
+        }
+
+        $this->entityManager->flush();
+        $output->writeln('Inventaire mis à jour avec succès.');
+
+        return Command::SUCCESS;
+    }
+
+    private function deduireMedicaments(Traitement $traitement, int $dose): void
+    {
+        foreach ($traitement->getMedicaments() as $medicament) {
+            $inventaire = $this->entityManager->getRepository(Inventaire::class)->findOneBy([
+                'user' => $traitement->getUser(),
+                'medicament' => $medicament,
+            ]);
+
+            if ($inventaire) {
+                $nouveauStock = max(0, $inventaire->getQuantite() - $dose);
+                $inventaire->setQuantite($nouveauStock);
+                $this->entityManager->persist($inventaire);
+            }
+        }
+    }
+}
